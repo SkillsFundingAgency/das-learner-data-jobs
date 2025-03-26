e596a79e4e7cf5f1e7079963908fdcd7794c164f
Paul Graham <paul.graham@coprime.co.uk>
2025-03-26 14:32:36 +0000

    add mock server

---
+++ src/SFA.DAS.LearnerData.MockServer/OuterApiBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using WireMock.Logging;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.LearnerData.MockServer;
+
+public class OuterApiBuilder
+{
+    private readonly WireMockServer _server;
+
+    public OuterApiBuilder(int port)
+    {
+        _server = WireMockServer.Start(new WireMockServerSettings
+        {
+            Port = port,
+            UseSSL = true,
+            StartAdminInterface = true,
+            Logger = new WireMockConsoleLogger(),
+        });
+    }
+
+    public static OuterApiBuilder Create(int port)
+    {
+        return new OuterApiBuilder(port);
+    }
+
+    public OuterApi Build()
+    {
+        return new OuterApi(_server);
+    }
+
+    public OuterApiBuilder WithNewLearnerEndpoint()
+    {
+        _server.Given(
+                Request.Create()
+                    .WithPath("/learners")
+                    .UsingPost()
+            )
+            .RespondWith(
+                Response.Create()
+                    .WithStatusCode(HttpStatusCode.Created));
+
+        return this;
+    }
+}
+
