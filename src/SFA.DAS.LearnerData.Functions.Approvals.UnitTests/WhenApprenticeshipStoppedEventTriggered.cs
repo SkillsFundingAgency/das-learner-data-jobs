9e498c5732e41a233b346f46b17bf333d71a0cb5
KUMAR, Santhosh <Santhosh.KUMAR@EDUCATION.GOV.UK>
2025-09-29 13:24:20 +0100

    add unit tests

--- src/SFA.DAS.LearnerData.Functions.Approvals.UnitTests/WhenApprenticeshipStoppedEventTriggered.cs
+++ src/SFA.DAS.LearnerData.Functions.Approvals.UnitTests/WhenApprenticeshipStoppedEventTriggered.cs
@@ -58,4 +58,34 @@ namespace SFA.DAS.LearnerData.Functions.Approvals.UnitTests;
                 x => x.PatchApprenticeshipId(It.IsAny<long>(), It.IsAny<long>(),
                     It.IsAny<PatchLearnerDataApprenticeshipIdRequest>()), Times.Never);
         }
-    }
+
+        [Test, MoqAutoData]
+        public async Task And_No_ApprenticeShipID_Then_swallow_event(
+        [Frozen] Mock<ILearnerDataJobsOuterApi> api,
+        HandleApprenticeshipStoppedEvent sut,
+        ApprenticeshipStoppedEvent evt)
+        {
+            evt.ApprenticeshipId = 0;
+
+            await sut.Handle(evt, null);
+
+            api.Verify(
+                x => x.PatchApprenticeshipId(It.IsAny<long>(), It.IsAny<long>(),
+                    It.IsAny<PatchLearnerDataApprenticeshipIdRequest>()), Times.Never);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_Not_IsWithdrawn_From_Start_Course_Then_swallow_event(
+            [Frozen] Mock<ILearnerDataJobsOuterApi> api,
+            HandleApprenticeshipStoppedEvent sut,
+            ApprenticeshipStoppedEvent evt)
+        {
+            evt.IsWithDrawnAtStartOfCourse = false;
+
+            await sut.Handle(evt, null);
+
+            api.Verify(
+                x => x.PatchApprenticeshipId(It.IsAny<long>(), It.IsAny<long>(),
+                    It.IsAny<PatchLearnerDataApprenticeshipIdRequest>()), Times.Never);
+        }
+}
